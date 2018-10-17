9f7b9f7504d3eeeabedd9a2aad8d1bd849937b84
Kevin Barabash <kevinb@khanacademy.org>
2018-10-16 23:13:34 -0400

    report coverage after each test instead after each render

--- packages/vite-demo/test/hidden-button.test.js
+++ packages/vite-demo/test/hidden-button.test.js
@@ -4,7 +4,9 @@ import HiddenButton from "../src/hidden-button.js";
 describe("HiddenButton", () => {
     test("it should render", async () => {     
         const element = await render(<HiddenButton/>);
+        const actions = driver.actions();
+        await actions.click(element).perform();
         const text = await element.getText();
-        expect(text).toBe("Click me!");
+        expect(text).toBe("clicked");
     });
 });
