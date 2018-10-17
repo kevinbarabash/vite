e0675c6fce48099570439cd29f6d9d20bf983376
Kevin Barabash <kevinb@khanacademy.org>
2018-10-17 00:22:04 -0400

    start writing tests that check whether a button is clickable or not

--- packages/vite-demo/src/hidden-button.js
+++ packages/vite-demo/src/hidden-button.js
@@ -1,7 +1,15 @@
 // @flow
 import * as React from "react";
+import {css, StyleSheet} from "aphrodite";
 
-export default class HiddenButton extends React.Component<{}> {
+type Props = {
+    cover: boolean,
+}
+export default class HiddenButton extends React.Component<Props> {
+    static defaultProps = {
+        cover: false,
+    }
+
     state = {
         clicked: false,
     }
@@ -11,8 +19,21 @@ export default class HiddenButton extends React.Component<{}> {
     }
 
     render() {
-        return <button onClick={this.handleClick}>
-            {this.state.clicked ? "clicked" : "no yet"}
-        </button>;
+        return <div>
+            <button onClick={this.handleClick}>
+                {this.state.clicked ? "clicked" : "no yet"}
+            </button>
+            <div className={css(this.props.cover && styles.cover)}/>
+        </div>;
     }
 }
+
+const styles = StyleSheet.create({
+    cover: {
+        position: "absolute",
+        left: 0,
+        top: 0,
+        width: 100,
+        height: 50,
+    },
+});
--- packages/vite-demo/test/hidden-button.test.js
+++ packages/vite-demo/test/hidden-button.test.js
@@ -2,11 +2,38 @@
 import HiddenButton from "../src/hidden-button.js";
 
 describe("HiddenButton", () => {
-    test("it should render", async () => {     
-        const element = await render(<HiddenButton/>);
+    test("it should respond to click without a cover", async () => {     
+        const container = await render(<HiddenButton />);
+
+        const button = await driver.findElement(By.tagName("button"));
+        const size = await button.getSize();
+        const location = await button.getLocation();
+        
         const actions = driver.actions();
-        await actions.click(element).perform();
-        const text = await element.getText();
+        await actions.mouseMove({
+            x: location.x + size.width / 2, 
+            y: location.y + size.height / 2,
+        }).click().perform();
+        
+        const text = await container.getText();
         expect(text).toBe("clicked");
     });
+
+    // TODO: fix babel plugin to handle settings props on JSX elements
+    // test("it should not respond to click with a cover", async () => {     
+    //     const container = await render(<HiddenButton cover={true} />);
+
+    //     const button = await driver.findElement(By.tagName("button"));
+    //     const size = await button.getSize();
+    //     const location = await button.getLocation();
+        
+    //     const actions = driver.actions();
+    //     await actions.mouseMove({
+    //         x: location.x + size.width / 2, 
+    //         y: location.y + size.height / 2,
+    //     }).click().perform();
+        
+    //     const text = await container.getText();
+    //     expect(text).toBe("clicked");
+    // });
 });
